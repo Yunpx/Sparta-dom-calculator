ad9e85ce4662be4f5571104f034c332ea96cc0a2
Yun <yxia@spartaglobal.com>
2018-07-03 22:22:59 +0100

    clear button works

--- js/calc.js
+++ js/calc.js
@@ -1,11 +1,18 @@
 
 var allButtons = document.getElementsByTagName("button");
+var clearButtons= document.getElementsByClassName('buttonClear');
+var currentText =document.getElementById("screen").textContent;
+
 var myString="";
 for (var i = 0; i < allButtons.length; i++) {
   allButtons[i].addEventListener("click",function () {
-    myString+=this.textContent;
-    document.getElementById("screen").textContent=myString;
-
-    console.log(this.textContent+" was Clicked.");
+    if(this.textContent==clearButtons[0].textContent){
+      myString = "";
+      document.getElementById("screen").textContent="0";
+    }else{
+      myString+=this.textContent;
+      document.getElementById("screen").textContent=myString;
+      console.log(this.textContent+" was Clicked.");
+    }
   })
 }
