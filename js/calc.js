80648f2f0e8b31a18eb4431215a5a5962b702488
Yun <yxia@spartaglobal.com>
2018-07-03 22:05:30 +0100

    screen displays full keypress

--- js/calc.js
+++ js/calc.js
@@ -1,7 +1,11 @@
 
 var allButtons = document.getElementsByTagName("button");
+var myString="";
 for (var i = 0; i < allButtons.length; i++) {
   allButtons[i].addEventListener("click",function () {
+    myString+=this.textContent;
+    document.getElementById("screen").textContent=myString;
+
     console.log(this.textContent+" was Clicked.");
   })
 }
