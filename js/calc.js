f95c7a2a4de4ce33a0122c214b398047a5bb4f6c
Yun <yxia@spartaglobal.com>
2018-07-03 21:39:24 +0100

    numpad input update

--- js/calc.js
+++ js/calc.js
@@ -1,12 +1,7 @@
 
 var allButtons = document.getElementsByTagName("button");
 for (var i = 0; i < allButtons.length; i++) {
-  allButtons[i].classList.add("numpad");
-}
-
-var myButtons = document.getElementsByClassName('numpad');
-for (var i = 0; i < myButtons.length; i++){
-  myButtons[i].addEventListener("click",function () {
+  allButtons[i].addEventListener("click",function () {
     console.log(this.textContent+" was Clicked.");
   })
 }
